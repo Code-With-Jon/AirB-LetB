690a709d5a3a293fe4657929109f5bcae92ee1e0
Jon <jon@Sciences-MacBook-Pro.local>
2019-12-05 18:11:43 -0800

    modified auth

--- routes/about.js
+++ routes/about.js
@@ -1,12 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect('/');
-}
 
-router.get('/', isLoggedIn, (req, res) => {
+
+router.get('/', (req, res) => {
     res.render('About/index', {
         user: req.user
     })
